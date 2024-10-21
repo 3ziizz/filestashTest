cc59daf6552f5cc0cf5b4c65d3e9262225450a02
Mohamed AbdelAziz <mohamed.abdelaziz1994@yahoo.com>
2024-10-21 13:04:02 +0300

    Update forkme.js

--- client/pages/connectpage/forkme.js
+++ client/pages/connectpage/forkme.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./forkme.scss";
 import { t } from "../../locales/";
 
-export const ForkMe = ({ repo = "https://www.google.com.eg/" }) => {
+export const ForkMe = ({ repo = "https://www.efinance.com.eg/service/efinance-cloud/" }) => {
     return (
         <div className="component_forkme">
-            <a href={repo} className="google-corner" aria-label="Visit Google" rel="nofollow" target="_blank">
+            <a href={repo} className="efinance-corner" aria-label="Visit eFinance Cloud" rel="nofollow" target="_blank">
                 <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg"
-                    alt="Google Logo"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZMcmeW33E6pMbqd_ax9vxCiVgH9GYarF2NA&s" // Replace with the correct logo URL
+                    alt="eFinance Cloud Logo"
                     width="80"
                     height="80"
                     style={{ borderRadius: "50%" }}
